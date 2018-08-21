10078289d4c7468651cf4735b96628237d8c7967
USER-PC\USER <cutyboy7@gmail.com>
2018-08-21 15:34:23 +0900

    MemberDao, memberMapper.xml 테스트 완료

--- FunDream/src/dao/IMemberDao.java
+++ FunDream/src/dao/IMemberDao.java
@@ -1,5 +1,16 @@
 package dao;
 
+import java.util.List;
+
+import model.Member;
+
 public interface IMemberDao {
+	
+	public int insertMember(Member member);
+	public List<Member> selectAllMember();
+	public Member selectOneMemberById(int m_id);
+	public Member selectOneMemberByEmail(String m_email);
+	public int updateMember(Member member);
+	public int deleteMember(int m_id);
 
 }
