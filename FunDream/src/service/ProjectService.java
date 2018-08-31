fd86f5f3c3a45f67898c12ab6d6a277ff5c0a977
USER-PC\USER <kym0812kr@gmail.com>
2018-08-31 14:17:25 +0900

    projectservice : 수정

--- FunDream/src/service/ProjectService.java
+++ FunDream/src/service/ProjectService.java
@@ -10,12 +10,29 @@ import model.Project;
 
 @Service
 public class ProjectService {
-	
+
 	@Autowired
 	private IProjectDao projectDao;
-	
-	public List<Project> getAllProjects(){
+
+	public List<Project> getAllProjects() {
 		return projectDao.selectAllProjects();
 	}
-	
+
+	// 최신 프로젝트 3개
+	public List<Project> getNewProject() {
+		List<Project> newlist = projectDao.selectNewProject();
+		return newlist;
+	}
+
+	// 마감임박 프로젝트 3개
+	public List<Project> getEndProject() {
+		List<Project> endlist = projectDao.selectEndProject();
+		return endlist;
+	}
+
+	// 목표 달성 완료 프로젝트 3개
+	public List<Project> getSuccessProject() {
+		List<Project> successlist = projectDao.selectSuccessProject();
+		return successlist;
+	}
 }
