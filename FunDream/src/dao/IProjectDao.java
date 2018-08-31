fbeb616e861120ee937c97c158805eec20e9c1eb
USER-PC\USER <kym0812kr@gmail.com>
2018-08-31 14:15:42 +0900

    IProject Dao : 추가

--- FunDream/src/dao/IProjectDao.java
+++ FunDream/src/dao/IProjectDao.java
@@ -7,5 +7,10 @@ import model.Project;
 public interface IProjectDao {
 	
 	public List<Project> selectAllProjects();
-
+	//최신순
+	public List<Project> selectNewProject();
+	//마감임박
+	public List<Project> selectEndProject();
+	//목표 달성 완료
+	public List<Project> selectSuccessProject();
 }
