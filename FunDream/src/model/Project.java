5be87b758eec7eb2d577a9dd395240ed3cde2a9e
USER-PC\USER <kym0812kr@gmail.com>
2018-08-31 14:52:05 +0900

    리워드 정책 -> 프로젝트 정책 모델 변경

--- FunDream/src/model/Project.java
+++ FunDream/src/model/Project.java
@@ -20,165 +20,193 @@ public class Project {
     private int p_calculate;
     private int p_type;
     private double per;
+    private String p_policy;
     
-    public double getPer() {
-		return per;
-	}
-
-	public void setPer(double per) {
-		this.per = per;
-	}
 
 	public Project() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Project(int p_index, int m_id, int p_approval, int ct_index, String p_name, Timestamp p_createdate, Timestamp p_startdate, Timestamp p_enddate, int p_target, int p_status, String p_s_link, String p_contents, String p_mainimg, int p_age, int p_calculate, int p_type) {
-		this.p_index = p_index;
-		this.m_id = m_id;
-		this.p_approval = p_approval;
-		this.ct_index = ct_index;
-		this.p_name = p_name;
-		this.p_createdate = p_createdate;
-		this.p_startdate = p_startdate;
-		this.p_enddate = p_enddate;
-		this.p_target = p_target;
-		this.p_status = p_status;
-		this.p_s_link = p_s_link;
-		this.p_contents = p_contents;
-		this.p_mainimg = p_mainimg;
-		this.p_age = p_age;
-		this.p_calculate = p_calculate;
-		this.p_type = p_type;
-	}
 
 	public int getP_index() {
 		return p_index;
 	}
 
+
 	public void setP_index(int p_index) {
 		this.p_index = p_index;
 	}
+
 
 	public int getM_id() {
 		return m_id;
 	}
 
+
 	public void setM_id(int m_id) {
 		this.m_id = m_id;
 	}
+
 
 	public int getP_approval() {
 		return p_approval;
 	}
 
+
 	public void setP_approval(int p_approval) {
 		this.p_approval = p_approval;
 	}
+
 
 	public int getCt_index() {
 		return ct_index;
 	}
 
+
 	public void setCt_index(int ct_index) {
 		this.ct_index = ct_index;
 	}
+
 
 	public String getP_name() {
 		return p_name;
 	}
 
+
 	public void setP_name(String p_name) {
 		this.p_name = p_name;
 	}
+
 
 	public Timestamp getP_createdate() {
 		return p_createdate;
 	}
 
+
 	public void setP_createdate(Timestamp p_createdate) {
 		this.p_createdate = p_createdate;
 	}
+
 
 	public Timestamp getP_startdate() {
 		return p_startdate;
 	}
 
+
 	public void setP_startdate(Timestamp p_startdate) {
 		this.p_startdate = p_startdate;
 	}
+
 
 	public Timestamp getP_enddate() {
 		return p_enddate;
 	}
 
+
 	public void setP_enddate(Timestamp p_enddate) {
 		this.p_enddate = p_enddate;
 	}
+
 
 	public int getP_target() {
 		return p_target;
 	}
 
+
 	public void setP_target(int p_target) {
 		this.p_target = p_target;
 	}
+
 
 	public int getP_status() {
 		return p_status;
 	}
 
+
 	public void setP_status(int p_status) {
 		this.p_status = p_status;
 	}
+
 
 	public String getP_s_link() {
 		return p_s_link;
 	}
 
+
 	public void setP_s_link(String p_s_link) {
 		this.p_s_link = p_s_link;
 	}
+
 
 	public String getP_contents() {
 		return p_contents;
 	}
 
+
 	public void setP_contents(String p_contents) {
 		this.p_contents = p_contents;
 	}
+
 
 	public String getP_mainimg() {
 		return p_mainimg;
 	}
 
+
 	public void setP_mainimg(String p_mainimg) {
 		this.p_mainimg = p_mainimg;
 	}
+
 
 	public int getP_age() {
 		return p_age;
 	}
 
+
 	public void setP_age(int p_age) {
 		this.p_age = p_age;
 	}
+
 
 	public int getP_calculate() {
 		return p_calculate;
 	}
 
+
 	public void setP_calculate(int p_calculate) {
 		this.p_calculate = p_calculate;
 	}
+
 
 	public int getP_type() {
 		return p_type;
 	}
 
+
 	public void setP_type(int p_type) {
 		this.p_type = p_type;
 	}
+
+
+	public double getPer() {
+		return per;
+	}
+
+
+	public void setPer(double per) {
+		this.per = per;
+	}
+
+
+	public String getP_policy() {
+		return p_policy;
+	}
+
+
+	public void setP_policy(String p_policy) {
+		this.p_policy = p_policy;
+	}
+
 
 	@Override
 	public String toString() {
@@ -186,6 +214,35 @@ public class Project {
 				+ ct_index + ", p_name=" + p_name + ", p_createdate=" + p_createdate + ", p_startdate=" + p_startdate
 				+ ", p_enddate=" + p_enddate + ", p_target=" + p_target + ", p_status=" + p_status + ", p_s_link="
 				+ p_s_link + ", p_contents=" + p_contents + ", p_mainimg=" + p_mainimg + ", p_age=" + p_age
-				+ ", p_calculate=" + p_calculate + ", p_type=" + p_type + ", per=" + per + "]";
+				+ ", p_calculate=" + p_calculate + ", p_type=" + p_type + ", per=" + per + ", p_policy=" + p_policy
+				+ "]";
 	}
+
+
+	public Project(int p_index, int m_id, int p_approval, int ct_index, String p_name, Timestamp p_createdate,
+			Timestamp p_startdate, Timestamp p_enddate, int p_target, int p_status, String p_s_link, String p_contents,
+			String p_mainimg, int p_age, int p_calculate, int p_type, double per, String p_policy) {
+		this.p_index = p_index;
+		this.m_id = m_id;
+		this.p_approval = p_approval;
+		this.ct_index = ct_index;
+		this.p_name = p_name;
+		this.p_createdate = p_createdate;
+		this.p_startdate = p_startdate;
+		this.p_enddate = p_enddate;
+		this.p_target = p_target;
+		this.p_status = p_status;
+		this.p_s_link = p_s_link;
+		this.p_contents = p_contents;
+		this.p_mainimg = p_mainimg;
+		this.p_age = p_age;
+		this.p_calculate = p_calculate;
+		this.p_type = p_type;
+		this.per = per;
+		this.p_policy = p_policy;
+	}
+	
+	
+
+
 }
--- FunDream/src/model/Reward.java
+++ FunDream/src/model/Reward.java
@@ -15,137 +15,156 @@ public class Reward {
     private String r_option;
     private int r_del;
     private int r_amt;
-    private String r_policy;
+    
     
     public Reward() {
 		// TODO Auto-generated constructor stub
 	}
-    
-    public Reward(int r_index, int p_index, int m_id, int r_price, String r_name, String r_img, String r_contents, int ct_index, Timestamp r_start, String r_option, int r_del, int r_amt, String r_policy) {
-		this.r_index = r_index;
-		this.p_index = p_index;
-		this.m_id = m_id;
-		this.r_price = r_price;
-		this.r_name = r_name;
-		this.r_img = r_img;
-		this.r_contents = r_contents;
-		this.ct_index = ct_index;
-		this.r_start = r_start;
-		this.r_option = r_option;
-		this.r_del = r_del;
-		this.r_amt = r_amt;
-		this.r_policy = r_policy;
-	}
+
 
 	public int getR_index() {
 		return r_index;
 	}
 
+
 	public void setR_index(int r_index) {
 		this.r_index = r_index;
 	}
+
 
 	public int getP_index() {
 		return p_index;
 	}
 
+
 	public void setP_index(int p_index) {
 		this.p_index = p_index;
 	}
+
 
 	public int getM_id() {
 		return m_id;
 	}
 
+
 	public void setM_id(int m_id) {
 		this.m_id = m_id;
 	}
+
 
 	public int getR_price() {
 		return r_price;
 	}
 
+
 	public void setR_price(int r_price) {
 		this.r_price = r_price;
 	}
+
 
 	public String getR_name() {
 		return r_name;
 	}
 
+
 	public void setR_name(String r_name) {
 		this.r_name = r_name;
 	}
+
 
 	public String getR_img() {
 		return r_img;
 	}
 
+
 	public void setR_img(String r_img) {
 		this.r_img = r_img;
 	}
+
 
 	public String getR_contents() {
 		return r_contents;
 	}
 
+
 	public void setR_contents(String r_contents) {
 		this.r_contents = r_contents;
 	}
+
 
 	public int getCt_index() {
 		return ct_index;
 	}
 
+
 	public void setCt_index(int ct_index) {
 		this.ct_index = ct_index;
 	}
+
 
 	public Timestamp getR_start() {
 		return r_start;
 	}
 
+
 	public void setR_start(Timestamp r_start) {
 		this.r_start = r_start;
 	}
+
 
 	public String getR_option() {
 		return r_option;
 	}
 
+
 	public void setR_option(String r_option) {
 		this.r_option = r_option;
 	}
+
 
 	public int getR_del() {
 		return r_del;
 	}
 
+
 	public void setR_del(int r_del) {
 		this.r_del = r_del;
 	}
+
 
 	public int getR_amt() {
 		return r_amt;
 	}
 
+
 	public void setR_amt(int r_amt) {
 		this.r_amt = r_amt;
 	}
 
-	public String getR_policy() {
-		return r_policy;
-	}
 
-	public void setR_policy(String r_policy) {
-		this.r_policy = r_policy;
-	}
-    
 	@Override
 	public String toString() {
 		return "Reward [r_index=" + r_index + ", p_index=" + p_index + ", m_id=" + m_id + ", r_price=" + r_price
 				+ ", r_name=" + r_name + ", r_img=" + r_img + ", r_contents=" + r_contents + ", ct_index=" + ct_index
-				+ ", r_start=" + r_start + ", r_option=" + r_option + ", r_del=" + r_del + ", r_amt=" + r_amt
-				+ ", r_policy=" + r_policy + "]";
+				+ ", r_start=" + r_start + ", r_option=" + r_option + ", r_del=" + r_del + ", r_amt=" + r_amt + "]";
 	}
+
+
+	public Reward(int r_index, int p_index, int m_id, int r_price, String r_name, String r_img, String r_contents,
+			int ct_index, Timestamp r_start, String r_option, int r_del, int r_amt) {
+		this.r_index = r_index;
+		this.p_index = p_index;
+		this.m_id = m_id;
+		this.r_price = r_price;
+		this.r_name = r_name;
+		this.r_img = r_img;
+		this.r_contents = r_contents;
+		this.ct_index = ct_index;
+		this.r_start = r_start;
+		this.r_option = r_option;
+		this.r_del = r_del;
+		this.r_amt = r_amt;
+	}
+    
+    
 }
