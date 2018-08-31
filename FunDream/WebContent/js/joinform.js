7d89d5ce5c21eade2b4d293c15efac6ef872df18
USER-PC\USER <kym0812kr@gmail.com>
2018-08-31 16:22:34 +0900

    회원가입 시 이미지 미리보기 수정

--- FunDream/WebContent/js/joinform.js
+++ FunDream/WebContent/js/joinform.js
@@ -276,5 +276,21 @@ $(function() {
 		alert('이미지링크 : ' + $('#m_img').val());
 		return true;
 	});
+	   //이미지 미리보기
+    $(function() {
+        $("#imgInp").on('change', function(){
+            readURL(this);
+        });
+    });
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+                $('#blah').attr('src', e.target.result);
+            }
+          reader.readAsDataURL(input.files[0]);
+        }
+    }
 
 });
